2c6cce5bc9881b96ef1e33bda02220edad7f9590
Test 2019/5/15 <1833956514@qq.com>
2019-05-15 11:54:51 +0800

    test 2019/5/15 11.48

--- src/main/java/com/feng/Hello.java
+++ src/main/java/com/feng/Hello.java
@@ -2,6 +2,10 @@ package com.feng;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        System.out.println("aaa1");
+    }
+    private void A1(){
+
+
     }
 }
